f04f2194f33e06f8877c32a4368d48d9cd1fee72
musextli-sudo <musextli@gmail.com>
2026-07-15 13:47:39 +0200

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,11 @@
+const vocabDatabase = {
+    "ep1": [
+        { word: "Flygplats", trans: "Airport" },
+        { word: "Tåg", trans: "Train" },
+        { word: "Skylt", trans: "Sign" }
+    ],
+    "ep2": [
+        { word: "Hej", trans: "Hello" },
+        { word: "Tack", trans: "Thanks" }
+    ]
+};
